a080920c6e32e602b7395cb3ff290b0f462b1360
AlexShtouff <alexkraft.new@gmail.com>
2025-05-19 08:22:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.expandable-item').forEach(item => {
         const subList = item.querySelector(':scope > .sub-styles');
-        if (subList) {
+        const arrow = item.querySelector('.arrow');
+
+        if (subList && arrow) {
             item.addEventListener('click', (e) => {
                 e.stopPropagation();
                 subList.classList.toggle('open');
-                item.classList.toggle('open');
+                arrow.textContent = subList.classList.contains('open') ? '↓' : '→';
             });
         }
     });
